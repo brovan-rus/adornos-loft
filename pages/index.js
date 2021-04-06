944ed7bc152d7a91a7ec75f7253034c04e136ff5
Konstantin Brovtsev <brovan@yandex.ru>
2021-04-06 03:44:17 +0300

    feat add all popups and started with js

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,31 @@
 const hamburger = document.querySelector('.hamburger');
 const mobileMenu = document.querySelector('.nav_device_mobile')
+const bookingButtonIntro = document.querySelector('.booking-button_place_intro')
+const introText = document.querySelectorAll('.intro_content_text');
+const bookingButtons = document.querySelectorAll('.booking-button');
+const popupWithForm = document.querySelector('.popup_content_booking-form')
+
 hamburger.addEventListener('click', handleHamburgerMenu);
+
+Array.from(bookingButtons).forEach((element) => {
+  element.addEventListener('click', openPopupWithForm);
+})
 
 function handleHamburgerMenu() {
   hamburger.classList.toggle('hamburger_active');
   mobileMenu.classList.toggle('nav_device_mobile_active');
 }
 
-const bookingButtonIntro = document.querySelector('.booking-button_place_intro')
-const introText = document.querySelectorAll('.intro_content_text');
+function openPopupWithForm() {
+  console.log(popupWithForm);
+  popupWithForm.classList.add('popup_opened');
+}
+
+
 bookingButtonIntro.addEventListener('mouseover', () => Array.from(introText).forEach((element) => {
   element.setAttribute('style', 'color:transparent');
-  console.log('down');
 }))
 
 bookingButtonIntro.addEventListener('mouseout', () => Array.from(introText).forEach((element) => {
   element.removeAttribute('style', 'color:transparent');
-  console.log('down');
 }))
-console.log(bookingButtonIntro);
