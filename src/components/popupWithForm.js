521a296a1175bcb73531c185737541d6cfdcb9e0
Konstantin Brovtsev <brovan@yandex.ru>
2021-04-14 19:52:42 +0300

    Add form sending functionality

--- src/components/popupWithForm.js
+++ src/components/popupWithForm.js
@@ -1,31 +1,38 @@
-import Popup from './popup.js';
+import Popup from "./popup.js";
 
 export default class PopupWithForm extends Popup {
   constructor(popupSelector, submit) {
     super(popupSelector);
     this._submit = submit;
-    this._popupForm = document.querySelector(popupSelector).querySelector('.form');
+    this._popupForm = document
+      .querySelector(popupSelector)
+      .querySelector(".form");
     this._handleFormSubmit = this._handleFormSubmit.bind(this);
+    this._sendButton = this._popupForm.querySelector(".form__submit-button");
+    this._sendButtonText = this._sendButton.textContent;
+    this.close = this.close.bind(this);
     this.setEventListeners();
   }
 
   form() {
-    return (this._popupForm);
+    return this._popupForm;
   }
 
   _getInputValues(form) {
-    const values = Array.from(form.querySelectorAll('.form__input'))
-      .map(({value}) => {
-        return value
-      });
-    const keys = Array.from(form.querySelectorAll('.form__input'))
-      .map(({name}) => {
-        return name
-      });
-    const inputValues = {}
+    const values = Array.from(form.querySelectorAll(".form__input")).map(
+      ({ value }) => {
+        return value;
+      }
+    );
+    const keys = Array.from(form.querySelectorAll(".form__input")).map(
+      ({ name }) => {
+        return name;
+      }
+    );
+    const inputValues = {};
     keys.forEach((element, i) => {
-      inputValues[element] = values[i]
-    })
+      inputValues[element] = values[i];
+    });
     return inputValues;
   }
 
@@ -33,7 +40,7 @@ export default class PopupWithForm extends Popup {
     evt.preventDefault();
     const inputValues = this._getInputValues(this._popupForm);
     this._submit(inputValues);
-    this.close();
+    setTimeout(this.close, 2000);
   }
 
   close() {
@@ -41,8 +48,18 @@ export default class PopupWithForm extends Popup {
     super.close();
   }
 
+  changeButtonText(buttonText) {
+    this._sendButton.textContent = buttonText;
+  }
+
+  toggleButtonState() {
+    if (this._sendButtonText === this._sendButton.textContent) {
+      this._sendButton.textContent = "Отправка...";
+    } else this._sendButton.textContent = this._sendButtonText;
+  }
+
   setEventListeners() {
-    this._popupForm.addEventListener('submit', this._handleFormSubmit);
+    this._popupForm.addEventListener("submit", this._handleFormSubmit);
     super.setEventListeners();
   }
 }
--- src/index.js
+++ src/index.js
@@ -17,8 +17,12 @@ import {
   introText,
 } from "./components/constants.js";
 import Popup from "./components/popup.js";
+import PopupWithForm from "./components/popupWithForm.js";
 import FormValidator from "./components/formValidator.js";
 import "./pages/index.css";
+
+import { Email } from "./components/smtp.js";
+
 //import { Viewer } from "photo-sphere-viewer";
 // import "photo-sphere-viewer/dist/photo-sphere-viewer.css";
 // import Page from "./components/Page.js";
@@ -46,8 +50,39 @@ const bookingFormValidator = new FormValidator(validationValues, bookingForm);
 bookingFormValidator.enableValidation();
 
 // Создаём экзмемляры классов с модальными окнами
-const popupWithForm = new Popup(".popup_content_booking-form");
-popupWithForm.setEventListeners();
+const popupWithForm = new PopupWithForm(
+  ".popup_content_booking-form",
+  (inputValues) => {
+    popupWithForm.toggleButtonState();
+    Email.send({
+      Host: "smtp.elasticemail.com",
+      Username: "brovan@gmail.com",
+      Password: "815DBF0EB5A96D942173188498531472E32C",
+      To: "brovan@yandex.ru",
+      From: "brovan@gmail.com",
+      Subject: `Заявка на бронирование лофта от пользователя ${inputValues.userName}`,
+      Body: `Отправлена зявка на дату ${inputValues.userName} &#x0A; на бронирование лофта &#010; дата бронирования - ${inputValues.bookingDate}. &#x0A; Телефонный номер - ${inputValues.phoneNumber}`,
+    })
+      .then((message) => {
+        popupWithForm.changeButtonText("Мы свяжемся с вами в ближайшее время!");
+      })
+      .catch((message) => {
+        popupWithForm.changeButtonText(
+          "Произошла ошибка при отправке формы..."
+        );
+      })
+      .finally(() => {
+        setTimeout(() => {
+          popupWithForm.toggleButtonState();
+        }, 2000);
+      });
+  }
+);
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 const popupWithImage = new Popup(".popup_content_photo");
 popupWithImage.setEventListeners();
 const popupWithFeachureCard = new Popup(".popup_content_feachure-card");
@@ -78,10 +113,27 @@ function handleOpenPopupWithForm() {
   popupWithForm.open();
 }
 
-function handleSubmitForm(evt) {
-  evt.preventDefault();
-  popupWithForm.close();
-}
+// function handleSubmitForm(evt) {
+//   evt.preventDefault();
+//
+//   // Email.send({
+//   //   Host: "smtp.elasticemail.com",
+//   //   Username: "brovan@gmail.com",
+//   //   Password: "815DBF0EB5A96D942173188498531472E32C",
+//   //   To: "brovan@yandex.ru",
+//   //   From: "brovan@gmail.com",
+//   //   Subject: "test",
+//   //   Body: "test",
+//   // })
+//   //   .then((message) => {
+//   //     alert(message);
+//   //   })
+//   //   .catch((message) => {
+//   //     alert(message);
+//   //   });
+//
+//   popupWithForm.close();
+// }
 
 function makeGalleryItem(url, title = "Интерьер лофта Адорнос") {
   const galleryItem = document.createElement("img");
@@ -129,13 +181,16 @@ Array.from(feachureCardImages).forEach((element) => {
 Array.from(bookingButtons).forEach((element) => {
   element.addEventListener("click", handleOpenPopupWithForm);
 });
-bookingForm.addEventListener("submit", handleSubmitForm);
+
+// bookingForm.addEventListener("submit", handleSubmitForm);
+
 // Убираем текст с видео при наведении на клавишу "Забронировать"
 bookingButtonIntro.addEventListener("mouseover", () =>
   Array.from(introText).forEach((element) => {
     element.setAttribute("style", "color:transparent");
   })
 );
+
 bookingButtonIntro.addEventListener("mouseout", () =>
   Array.from(introText).forEach((element) => {
     element.removeAttribute("style", "color:transparent");
